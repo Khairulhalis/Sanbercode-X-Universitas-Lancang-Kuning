cea0afd14dc84a7d353b083440b5b18695cee958
khairulhalis <kholissugara@gmail.com>
2024-05-28 21:57:19 +0700

    latihan 12

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -10,9 +10,18 @@ use App\Http\Controllers\HomeController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/register', [AuthController::class, 'showRegisterForm']);
+Route::post('/welcome', [AuthController::class, 'welcome']);
+
+Route::get('/data-table',function(){
+    return view('page.data-table');
+});
+Route::get('/table', function () {
+    return view('page.table');
+});
